b6a10cba99ed97460221bd535122197a7a436fa1
safi-sfn <mdsafiruddin24@gmail.com>
2025-06-17 16:58:00 +0530

    entity: create studentInfo model

--- HibernateLOBs/src/main/java/com/learn/model/StudentInfo.java
+++ HibernateLOBs/src/main/java/com/learn/model/StudentInfo.java
@@ -1,8 +1,73 @@
 package com.learn.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 
 @Entity
 public class StudentInfo {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer sid;
+	
+	private String sName;
+	
+	private String sCity;
+	
+	@Lob
+	@Column(length=100000)
+	private byte[] image;
+	@Lob
+	private char[] textFile;
+	
+	public StudentInfo() {
+		System.out.println("Constructor of StudentInfo");
+	}
+	
+	
+	public byte[] getImage() {
+		return image;
+	}
 
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+
+	public char[] getTextFile() {
+		return textFile;
+	}
+
+	public void setTextFile(char[] textFile) {
+		this.textFile = textFile;
+	}
+
+
+	public Integer getSid() {
+		return sid;
+	}
+
+	public void setSid(Integer sid) {
+		this.sid = sid;
+	}
+
+	public String getsName() {
+		return sName;
+	}
+
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
+
+	public String getsCity() {
+		return sCity;
+	}
+
+	public void setsCity(String sCity) {
+		this.sCity = sCity;
+	}
+	
 }
