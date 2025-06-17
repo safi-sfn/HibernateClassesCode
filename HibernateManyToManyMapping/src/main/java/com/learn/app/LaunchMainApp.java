5401d39981e45c38689e9d43b9674f1b9f312848
safi-sfn <mdsafiruddin24@gmail.com>
2025-06-17 11:34:21 +0530

    feat: add data retrieval code

--- HibernateManyToManyMapping/src/main/java/com/learn/app/LaunchMainApp.java
+++ HibernateManyToManyMapping/src/main/java/com/learn/app/LaunchMainApp.java
@@ -28,55 +28,66 @@ public class LaunchMainApp {
 		sessionFactory = config.buildSessionFactory();
 		session = sessionFactory.openSession();
 
-		Courses c1 = new Courses();
-		c1.setId(1);
-		c1.setCourseName("Java Full-Stack");
-		c1.setCoursePrice(3999);
+//		Courses c1 = new Courses();
+//		c1.setId(1);
+//		c1.setCourseName("Java Full-Stack");
+//		c1.setCoursePrice(3999);
+//		
+//		Courses c2 = new Courses();
+//		c2.setId(2);
+//		c2.setCourseName("Web Development");
+//		c2.setCoursePrice(2499);
+//		
+//		Courses c3 = new Courses();
+//		c3.setId(3);
+//		c3.setCourseName("DevOps");
+//		c3.setCoursePrice(999);
+//		
+//		Set<Courses> courseSet1 = new HashSet<>();
+//		courseSet1.add(c1);
+//		courseSet1.add(c2);
+//		courseSet1.add(c3);
+//		
+//		Set<Courses> courseSet2 = new HashSet<>();
+//		courseSet2.add(c1);
+//		courseSet2.add(c3);
+//		
+//		Students s1 = new Students();
+//		s1.setId(1);
+//		s1.setName("Arun");
+//		s1.setCity("Delhi");
+//		s1.setCourses(courseSet1);
+//		
+//		Students s2 = new Students();
+//		s2.setId(2);
+//		s2.setName("Vivek");
+//		s2.setCity("Patna");
+//		s2.setCourses(courseSet2);
+//		
+//		Students s3 = new Students();
+//		s3.setId(3);
+//		s3.setName("Karim");
+//		s3.setCity("Pune");
+//		s3.setCourses(courseSet1);
 		
-		Courses c2 = new Courses();
-		c2.setId(2);
-		c2.setCourseName("Web Development");
-		c2.setCoursePrice(2499);
+		Students stu1 = session.get(Students.class, 1);
+		System.out.println(stu1);
+		System.out.println("*************************");
 		
-		Courses c3 = new Courses();
-		c3.setId(3);
-		c3.setCourseName("DevOps");
-		c3.setCoursePrice(999);
+		Students stu2 = session.get(Students.class, 2);
+		System.out.println(stu2);
+		System.out.println("*************************");
 		
-		Set<Courses> courseSet1 = new HashSet<>();
-		courseSet1.add(c1);
-		courseSet1.add(c2);
-		courseSet1.add(c3);
-		
-		Set<Courses> courseSet2 = new HashSet<>();
-		courseSet2.add(c1);
-		courseSet2.add(c3);
-		
-		Students s1 = new Students();
-		s1.setId(1);
-		s1.setName("Arun");
-		s1.setCity("Delhi");
-		s1.setCourses(courseSet1);
-		
-		Students s2 = new Students();
-		s2.setId(2);
-		s2.setName("Vivek");
-		s2.setCity("Patna");
-		s2.setCourses(courseSet2);
-		
-		Students s3 = new Students();
-		s3.setId(3);
-		s3.setName("Karim");
-		s3.setCity("Pune");
-		s3.setCourses(courseSet1);
+		Students stu3 = session.get(Students.class, 3);
+		System.out.println(stu3);
 		
 		
 		
 		try {
 			transaction = session.beginTransaction();
-			session.persist(s1);
-			session.persist(s2);
-			session.persist(s3);
+//			session.persist(s1);
+//			session.persist(s2);
+//			session.persist(s3);
 			flag=true;
 		}
 		catch(HibernateException e) {
